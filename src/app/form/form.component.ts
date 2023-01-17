5d739302f2f03fe7c61c18d384684b9e9ce4089f
Emily LeBlanc <emily.leblanc@aircanada.ca>
2023-01-17 12:17:00 -0500

    read me

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+  submit(values: any){
+    console.log('form submitted', values)
+  }
+}
